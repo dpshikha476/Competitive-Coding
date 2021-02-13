8cf793cb2d0a24f872555a5bfe27243a8f741a26
Deep Shikha <71020381+dpshikha476@users.noreply.github.com>
2021-02-13 23:30:27 +0530

    Day 44

---
+++ Hackerearth/Print the Numbers.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+class TestClass 
+{
+    public static void main(String args[] ) throws Exception 
+    {
+       
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt(); 
+        while(n>0)
+        {
+            int i = s.nextInt();
+            System.out.print(i+ " ");  
+            n--;
+        }               
+          
+
+    }
+}
