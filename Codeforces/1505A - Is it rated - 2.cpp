7704227767dbc1ebc11f558fc7c94d1a13280f55
Deep Shikha <71020381+dpshikha476@users.noreply.github.com>
2021-04-01 21:39:50 +0530

    Day 91

---
+++ Codeforces/1505A - Is it rated - 2.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include<string.h>
+using namespace std;
+ 
+int main()
+{
+  std::string lols;
+    while(!(std::getline(cin,lols)).eof()) 
+    { 
+        cout<<"NO"<<"\n";
+    }
+    cout<<"NO"<<"\n";
+}
