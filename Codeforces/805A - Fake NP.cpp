344c4e92df96df5e9640bf9302bd2dbb69d1f6fb
Deep Shikha <71020381+dpshikha476@users.noreply.github.com>
2021-04-21 21:41:58 +0530

    Day 111

---
+++ Codeforces/805A - Fake NP.cpp
@@ -0,0 +1,21 @@
+
+#include<bits/stdc++.h>
+#include<string.h>
+#include<vector>
+using namespace std;
+typedef long long int lli;
+ 
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    lli l,r,a=0,b=0,i;
+    cin>>l>>r;
+    if(l==r)
+    cout<<l;
+    else
+    cout<<2;
+}
