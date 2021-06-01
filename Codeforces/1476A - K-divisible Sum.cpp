538e95ed734c0b4ab690939d40c4a1868bc5be05
Deep Shikha <71020381+dpshikha476@users.noreply.github.com>
2021-06-01 20:17:48 +0530

    Day 152

--- Codeforces/1476A - K-divisible Sum.cpp
+++ Codeforces/1476A - K-divisible Sum.cpp
@@ -1,14 +1,19 @@
 #include<bits/stdc++.h>
+
 using namespace std;
-int main()
+
+int main() 
 {
-  int t;
-  cin>>t;
-  while(t--)
+  int t; cin >> t;
+  while(t--) 
   {
-    long long int n,k,a;
-    cin>>n>>k;
-    a=(n+k-1)/k*k;
-    cout<<(a+n-1)/n<<endl;
-  }
+    long long n, k;
+    cin >> n >> k;
+    
+    long long cf = (n + k - 1) / k;
+    k *= cf;
+    
+    cout << (k + n - 1) / n << endl;
+   }
+  return 0;
 }
