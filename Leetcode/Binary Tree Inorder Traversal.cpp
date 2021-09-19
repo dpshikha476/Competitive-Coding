c0490c51ea76b267d2d39e1a4a0fe1ebfc37cc33
Deep Shikha <71020381+dpshikha476@users.noreply.github.com>
2021-09-19 17:16:19 +0530

    Day 262

---
+++ Leetcode/Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,19 @@
+ void in(TreeNode* root, vector<int> &v)
+    {
+        if(root==NULL)
+            return;
+        
+        in(root->left,v);
+        v.push_back(root->val);
+        in(root->right,v);
+          
+    }
+    
+    vector<int> inorderTraversal(TreeNode* root) 
+    {
+        vector<int> v;
+        
+        in(root,v);
+        
+        return v;
+    }
