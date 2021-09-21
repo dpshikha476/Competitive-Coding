4a9e0300ad4a6b478c5b0240bcca9968f2abff79
Deep Shikha <71020381+dpshikha476@users.noreply.github.com>
2021-09-21 15:30:27 +0530

    Day 264

---
+++ Leetcode/Invert Binary Tree.cpp
@@ -0,0 +1,14 @@
+ 
+    TreeNode* invertTree(TreeNode* root) 
+    {
+        if(root==NULL)
+            return root;
+        
+        swap(root->left,root->right);
+        
+        invertTree(root->left);
+        invertTree(root->right);
+        
+        return root;
+        
+    }
---
+++ Leetcode/Path Sum.cpp
@@ -0,0 +1,12 @@
+bool hasPathSum(TreeNode* root, int targetSum) 
+    {
+        if(root==NULL)
+            return false;
+        
+        if(root->left==NULL && root->right==NULL && root->val==targetSum)
+            return true;
+        
+         return hasPathSum(root->left,targetSum-root->val) || 
+             hasPathSum(root->right,targetSum-root->val);
+        
+    }
