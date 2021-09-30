b078311af2c30fcc82c3c4dcea458c6554d53ee8
Deep Shikha <71020381+dpshikha476@users.noreply.github.com>
2021-09-30 16:26:53 +0530

    Day 273

---
+++ Interview bit/Set Matrix Zeros.cpp
@@ -0,0 +1,26 @@
+void Solution::setZeroes(vector<vector<int> > &A) 
+{
+    map<int,int>c,r;
+    
+    for(int i=0;i<A.size();i++)
+    {
+        for(int j=0;j<A[0].size();j++)
+        {
+            if(A[i][j]==0)
+            {
+                c[j]=1;
+                r[i]=1;
+            }
+        }
+    }
+    for(int i=0;i<A.size();i++)
+    {
+        for(int j=0;j<A[0].size();j++)
+        {
+            if(c[j]==1 || r[i]==1)
+            {
+                A[i][j]=0;
+            }
+        }
+    }
+}
