fd6a4864c25ef9058448c1bc84544b1bdaebba78
Deep Shikha <71020381+dpshikha476@users.noreply.github.com>
2021-10-09 15:48:20 +0530

    Day 282

---
+++ Interview bit/Power Of Two Integers.cpp
@@ -0,0 +1,15 @@
+int Solution::isPower(int a) 
+{
+    if (a == 1)
+        return true;
+
+
+    for (int i = 2; i * i <= a; i++) 
+    {
+        double val = log(a) / log(i);
+        if ((val - (int)val) < 0.00000001)
+            return true;
+    }
+
+    return false;  
+}
