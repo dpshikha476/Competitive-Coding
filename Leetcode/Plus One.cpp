a254240bba90df3d3e5f6d2cd17bc5dbd9be45e8
Deep Shikha <71020381+dpshikha476@users.noreply.github.com>
2021-11-16 21:04:01 +0530

    Day 320

---
+++ Leetcode/Plus One.cpp
@@ -0,0 +1,29 @@
+    vector<int> plusOne(vector<int>& digits) 
+    {
+      vector<ll> ans;
+        ll flag = 0;
+        for(ll i = digits.size()-1;i>=0;i--)
+        {
+            ll val = digits[i] + 1;
+            if(val<=9)
+            {
+                digits[i] = val;
+                return digits;
+            }
+            else
+            {
+                if(i==0 and val>9)
+                {
+                    flag = 1;
+                }
+                digits[i] = 0;
+                
+            }
+        }
+        
+        if(flag==1)
+        {
+           digits.insert(digits.begin(),1);
+            }
+        return digits;   
+    }
